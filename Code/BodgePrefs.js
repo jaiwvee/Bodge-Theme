6d5294f200cdfb64940faaaf1deda0bf603020b3
jaiwvee <jaiwvee@gmail.com>
2014-01-29 22:44:44 -0800

    Updated in v.0.0.23 alpha
    Added preview.png and all apps view

--- Code/BodgePrefs.js
+++ Code/BodgePrefs.js
@@ -1,4 +1,6 @@
-// WEATHERVIEW
-var Locale = 'los angeles,ca'; // weather location ('city,state', zipcode, or citycode)
-var Scale = 'f'; // f = fahrenheit, c = celsius, anything else = kelvin
+
+// PLEASE NOTE YOU MUST RESPRING (CAN ALSO REBOOT) FOR ANY CHANGES TO TAKE EFFECT
+
+var Locale = 'Cupertino,Ca';
+var Scale = 'f';
 var W_Reload = 10; // time (in minutes) until weather data reloads
